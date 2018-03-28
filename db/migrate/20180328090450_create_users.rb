0a69e506c3fa42a0fadee21593ab396e7e0e232e
RODOLFO <rodolfocarreras@gmail.com>
2018-03-28 11:13:53 +0200

    migrate

--- db/migrate/20180328090450_create_users.rb
+++ db/migrate/20180328090450_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.stirng :name
+      t.string :name
       t.references :team, foreign_key: true
       t.string :avatar
 
